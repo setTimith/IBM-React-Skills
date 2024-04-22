c7b7e062ce3fbdd1f8572f3796717352be73d63b
setTimith <thomasabboud0@gmail.com>
2024-04-22 22:49:50 +0300

    Note Update

--- react-dog-breed-api/src/index.js
+++ react-dog-breed-api/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
 
-// This is how it it is done from React 18 onwards
+// This is how it it is done standardly from React 18 onwards
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App color="green" size="20px" />);
