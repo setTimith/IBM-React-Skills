8601a3f2ab03d12851a42bd6fd64acfd1d53a2a8
setTimith <thomasabboud0@gmail.com>
2024-04-24 21:59:14 +0300

    Styling Change

--- react-dog-breed-api/src/index.js
+++ react-dog-breed-api/src/index.js
@@ -5,4 +5,4 @@ import { createRoot } from "react-dom/client";
 // This is how it it is done standardly from React 18 onwards
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App color="green" size="20px" />);
+root.render(<App color="white" size="20px" />);
